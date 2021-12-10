11b7da1effa3a8744932dd090926ee998222cd3c
JustinBear99 <justin850509@gmail.com>
2021-12-10 21:03:13 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# PPOCR app
+This app is developed with pretrained models from [PPOCRv2](https://github.com/PaddlePaddle/PaddleOCR) using [Streamlit](https://streamlit.io/).
+
+## Usage
+
+Go to the app hosted by [Streamlit Cloud]()
+
+or
+
+### Install
---
+++ app.py
@@ -0,0 +1,33 @@
+import tempfile
+import streamlit as st
+import tools.infer.utility as utility
+
+from tools.infer.predict_system import inference
+
+def main():
+    st.title('Paddle OCR')
+    st.header('Upload your image')
+    image = st.file_uploader('', type=['png', 'jpg', 'jpeg'])
+    
+    if image is not None:
+        img = tempfile.NamedTemporaryFile(delete=True)
+        img.write(image.read())
+
+        st.image(image)
+        args = utility.parse_args()
+        args.vis_font_path = 'tools/msjh.ttf'
+        args.image_dir = img.name
+        pred_image, texts = inference(args)
+
+        st.header('Inference result')
+        st.image(pred_image)
+        st.subheader('Texts')
+        for line in texts:
+            st.code(line, language=None)
+    
+    st.header('Reference')
+    st.markdown('This app is developed with [PP-OCRv2](https://github.com/PaddlePaddle/PaddleOCR) pretrained model.')
+
+
+if __name__ == "__main__":
+    main()
